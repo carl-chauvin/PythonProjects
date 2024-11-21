15a66e85f4dd14348b053295f37344b7bd22be3b
Hello Code <109740156+carl-chauvin@users.noreply.github.com>
2024-11-20 21:30:41 -0500

    Update csvreader.py

--- Automated Task/csvreader/csvreader.py
+++ Automated Task/csvreader/csvreader.py
@@ -3,7 +3,7 @@ import csv
 
 #Abre el archivo 'receivers.csv' en modo lectura ('r')
 with open('receivers.csv', 'r') as archivo:
-    leer = csv.reader(archivo) # Lee el archivo CSV utilizando el lector CSV de Python
+    leer = csv.reader(archivo) # Lee el archivo CSV utilizando el metodo .reader de Python
     #next(leer) # si queremos saltar la primera fila
 
     for fila in leer: #itera sobre cada fila, extrayendo los datos.
